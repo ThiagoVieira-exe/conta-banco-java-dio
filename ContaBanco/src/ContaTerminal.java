2b52101b9afcce8f5d847e46b177953c5beaf61b
Thiago Vieira <devthiagovieira@gmail.com>
2025-02-16 21:12:00 -0300

    Lógica implementada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Por favor, digite o número da Conta !");
+    int conta = sc.nextInt();
+    sc.nextLine(); // Consome a quebra de linha deixada pelo nextInt()
+
+    System.out.println("Agora digite o número da Agência!");
+    String agencia = sc.next();
+    sc.nextLine(); // Consome a quebra de linha deixada pelo next()
+
+    System.out.println("Agora digite seu Nome Completo");
+    String nomeCompleto = sc.nextLine(); // Agora funcionará corretamente
+
+    System.out.println("Agora digite seu Saldo");
+    double saldo = sc.nextDouble();
+
+    System.out.printf(
+        "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+        nomeCompleto, agencia, conta, saldo);
+
+    sc.close();
   }
 }
